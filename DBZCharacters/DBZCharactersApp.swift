7acc134fc6345f137a8aaad6c3f661a83e5dc4fd
JAIRO-99 <jairo.lcelis1999@gmail.com>
2024-03-26 16:22:43 -0500

    Initial Commit

---
+++ DBZCharacters/DBZCharactersApp.swift
@@ -0,0 +1,17 @@
+//
+//  DBZCharactersApp.swift
+//  DBZCharacters
+//
+//  Created by New on 26/03/24.
+//
+
+import SwiftUI
+
+@main
+struct DBZCharactersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
